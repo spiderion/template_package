2018fbd158d6bce3a95f28f022c702bd9c04207d
ionolaru <spideiron@live.it>
2021-05-26 22:20:13 +0200

    - clean up logger

--- lib/logger/logger.dart
+++ lib/logger/logger.dart
@@ -3,16 +3,23 @@ import 'package:logger/logger.dart';
 class LoggerDefault {
   static Logger? _logger;
 
-  static Logger  get log {
+  static Logger get log {
     if (_logger == null) {
-      _logger = Logger( printer: PrettyPrinter(
-          methodCount: 1, // number of method calls to be displayed
-          errorMethodCount: 5, // number of method calls if stacktrace is provided
-          lineLength: 500, // width of the output
-          colors: false, // Colorful log messages
-          printEmojis: true, // Print an emoji for each log message
-          printTime: true // Should each log print contain a timestamp
-      ),);
+      _logger = Logger(
+        printer: PrettyPrinter(
+            methodCount: 1,
+            // number of method calls to be displayed
+            errorMethodCount: 5,
+            // number of method calls if stacktrace is provided
+            lineLength: 500,
+            // width of the output
+            colors: false,
+            // Colorful log messages
+            printEmojis: true,
+            // Print an emoji for each log message
+            printTime: true // Should each log print contain a timestamp
+            ),
+      );
     }
     return _logger!;
   }
