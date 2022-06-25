2a351c1acd462fce35949c3ca7c61e07c584fd0b
IonOlaru89 <ion.olaru@projectolympus.co.za>
2022-06-25 10:06:45 +0100

    add simple test

--- test/template_package_test.dart
+++ test/template_package_test.dart
@@ -1,1 +1,20 @@
+import 'package:template_package/template_package.dart';
+import 'package:flutter_test/flutter_test.dart';
 
+main() {
+  late Error sut;
+
+  setUp(() {
+    sut = BusinessError(message: "Error");
+  });
+
+   test('Test error', () {
+       //Arrange
+       final expected = 'Error';
+       //Act
+       final actual = sut.message;
+       //Assert
+       expect(actual, expected);
+     });
+
+}
